a60e05fc57a6c5b5925f3eb6163a23860439accc
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-12-12 04:22:50 +0100

    [WIP] Add levels endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const lessonsController = require('../controllers').lessons;
 const levelsController = require('../controllers').levels;
 
+const Lesson = require('../models').Lesson;
+const Levels = require('../models').LevelsAssociation;
+
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: "Welcome"
@@ -10,4 +13,43 @@ module.exports = (app) => {
   app.post('/api/lessons', lessonsController.create);
 
   app.post('/api/lessons/:id/level', levelsController.create);
+
+  app.get('/api/levels', function (req, res){
+    return Levels
+      .findAll({
+        attributes: ['level', Levels.sequelize.col('level')],
+        group: ['LevelsAssociation.level']
+      })
+      .then((data) => {
+        res.status(201).send(data);
+      })
+      .catch(error => {
+        res.status(400).send(error);
+      });
+  });
+
+  app.get('/api/levels/:id', function(req, res) {
+    return Levels
+      .findAll({
+        where: {
+          level: req.params.id,
+        },
+        include: {
+          model: Lesson,
+        }
+      })
+      .then((data) => {
+        const lessons = [];
+        data.forEach(level => {
+          lessons.push(level.Lesson);
+        });
+        res.status(201).send({
+          level: req.params.id,
+          lessons: lessons,
+        });
+      })
+      .catch(error => {
+        res.status(400).send(error);
+      });
+  });
 };
