d894fe38cc8ebbe8cb9a2f6321af0d16626933d5
Adrian Zniszczyński <a.zniszczynski@nowaera.pl>
2017-12-12 12:55:02 +0100

    Move catalog structure folder abowe

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-require('./server/routes')(app);
+require('./routes')(app);
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
